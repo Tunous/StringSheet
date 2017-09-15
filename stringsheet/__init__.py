c0b12aa0cacbd99ff4f9794297a89ad9b92aed0a
Łukasz Rutkowski <lukus178@gmail.com>
2017-09-15 20:57:57 +0200

    Bump version

--- stringsheet/__init__.py
+++ stringsheet/__init__.py
@@ -2,7 +2,7 @@
 __title__ = 'stringsheet'
 __description__ = 'Manage Android translations using Google Spreadsheets'
 __url__ = 'https://github.com/Tunous/StringsSheet'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 __author__ = 'Łukasz Rutkowski'
 __author_email__ = 'lukus178@gmail.com'
 __license__ = 'MIT'
